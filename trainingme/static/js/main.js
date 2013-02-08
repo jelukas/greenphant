b55f003bb66f5f8f6219fe8e48a99248d0a7fe77
Jesus Lucas <jelukas89@gmail.com>
2013-02-08 12:58:49 +0100

    Improved player and Styles

---
+++ trainingme/static/js/main.js
@@ -0,0 +1,24 @@
+$(document).on('ready',document_ready);
+
+var myPlayer = null;
+_V_("video_course").ready(function(){
+
+    myPlayer = this;    // Store the video object
+    var aspectRatio = 9/16; // Make up an aspect ratio
+
+    function resizeVideoJS(){
+        // Get the parent element's actual width
+        var width = document.getElementById(myPlayer.id).parentElement.offsetWidth;
+        // Set width to fill parent element, Set height
+        myPlayer.width(width).height( width * aspectRatio );
+    }
+
+    resizeVideoJS(); // Initialize the function
+    window.onresize = resizeVideoJS; // Call the function on resize
+});
+
+function document_ready(){
+    $('#freeVideo').on('hidden', function () {
+        myPlayer.pause()
+    })
+}
