d8bf10a0820c598c96a9b79c0bfba508ce4e2051
Jesus Lucas <jelukas89@gmail.com>
2013-02-13 12:58:57 +0100

    Pages done

--- trainingme/trainingme/urls.py
+++ trainingme/trainingme/urls.py
@@ -20,8 +20,11 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     #Static pages
+    (r'^pages/faqs/$',TemplateView.as_view(template_name="pages/faqs.html")),
+    (r'^pages/mission/$',TemplateView.as_view(template_name="pages/mission.html")),
+    (r'^pages/team/$',TemplateView.as_view(template_name="pages/team.html")),
     (r'^pages/terms_and_conditions/$',TemplateView.as_view(template_name="pages/terms.html")),
-    (r'^pages/privacy_policy/$',TemplateView.as_view(template_name="pages/privacy.html")),
+    (r'^pages/privacy_policy/$',TemplateView.as_view(template_name="pages/privacy_policy.html")),
     (r'^pages/enjoy-learning/$',TemplateView.as_view(template_name="pages/aprendiendo.html")),
     (r'^pages/enjoy-teaching/$',TemplateView.as_view(template_name="pages/ensenando.html")),
     (r'^pages/enjoy-helping/$',TemplateView.as_view(template_name="pages/ayudando.html")),
