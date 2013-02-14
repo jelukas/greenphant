ab8e28fc0860998def905b9d37d3c9e66f013467
Jesus Lucas <jelukas89@gmail.com>
2013-02-14 10:54:19 +0100

    Few fixes done

--- trainingme/personal/admin.py
+++ trainingme/personal/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from personal.models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import Profile
 
+
+class UserAdmin(UserAdmin):
+    list_display = ('username','email','first_name','last_name','is_active','date_joined')
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
 admin.site.register(Profile)
