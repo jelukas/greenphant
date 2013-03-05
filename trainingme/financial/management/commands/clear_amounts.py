f3d0c254e0031abd3afb3d5eea144b067376164f
Jesus Lucas <jelukas89@gmail.com>
2013-03-05 09:23:58 +0100

    Fixed Beta button

--- trainingme/financial/management/commands/clear_amounts.py
+++ trainingme/financial/management/commands/clear_amounts.py
@@ -1,4 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext as _
+from django.contrib.auth.models import User
 from financial.models import Order, Billing
 from datetime import datetime, timedelta
 from decimal import Decimal
@@ -8,14 +13,34 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         now = datetime.now()
-        orders = Order.objects.filter(created_at__lte= now - timedelta(days=30),datetime_cleared=None)
+        orders = Order.objects.filter(datetime_cleared=None)
+#        orders = Order.objects.filter(created_at__lte= now - timedelta(days=30),datetime_cleared=None)
+        teachers = {}
         if orders.count() > 0:
             for order in orders:
-                billing = order.get_seller().billing
-                billing.balance += Decimal(order.amount)*Decimal(str(0.7))
-                billing.save()
+                teacher = order.get_seller()
+                billing = teacher.billing
+                amount = Decimal(order.amount)*Decimal(str(0.7))
+                billing.balance += amount
+#                billing.save()
                 order.datetime_cleared = now
-                order.save()
-                print order
-                #An email should be sent
+#                order.save()
+                if not teachers.has_key(str(teacher.id)):
+                    teachers[str(teacher.id)] = { 'updated_balance': amount }
+                else:
+                    teachers[str(teacher.id)]['updated_balance'] += amount
+
+        self.send_mail_teachers(teachers)
         self.stdout.write( str(orders.count()) + ' Orders cleared \n')
+
+
+    def send_mail_teachers(self, teachers):
+        for id in teachers.keys():
+            teacher = User.objects.get(pk=id)
+            ctx_dict = {'teacher': teacher,'updated_amount': teachers[id]['updated_balance']}
+            #Send Email to the Teacher
+            message = render_to_string('financial/clear_amount_email.html',ctx_dict)
+            subject = _('TrainingMe.net - Notice: Amount Cleared to your balance')
+            msg = EmailMessage(subject, message, settings.DEFAULT_FROM_EMAIL, [teacher.email])
+            msg.content_subtype = "html"  # Main content is now text/html
+            msg.send()
