19ce51335325649a3173cba2ec1f73a914d4f573
Jesus Lucas <jelukas89@gmail.com>
2013-03-07 14:44:04 +0100

    Profile of teacher in list of courses

--- trainingme/personal/admin.py
+++ trainingme/personal/admin.py
@@ -7,6 +7,10 @@ from .models import Profile
 class UserAdmin(UserAdmin):
     list_display = ('username','email','first_name','last_name','is_active','date_joined')
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','subtitle','phone_number','linkedin_url',)
+    
+
 admin.site.unregister(User)
 admin.site.register(User,UserAdmin)
-admin.site.register(Profile)
+admin.site.register(Profile,ProfileAdmin)
