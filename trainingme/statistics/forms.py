aab1fb38c8872ac1ca8b53f4c3d416f46ed389df
Jesus Lucas <jelukas89@gmail.com>
2013-03-13 09:00:08 +0100

    Free Courses Style

--- trainingme/statistics/forms.py
+++ trainingme/statistics/forms.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext as _
 from .models import Interest
 
 class InterestList(forms.ModelForm):
-    category = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,label=_('What type of courses interest you ?'))
+    category = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,label=_('Tell us what category of courses interest you and you will be notified when they are published. If you\'re one of the top 5 in enrolling in a course, you can do for free to evaluate it.'))
 
     class Meta:
         model = Interest
