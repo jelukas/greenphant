6f7dd6dc59bd1ffac793ff69debc1a2b1f7e9114
Jesus Lucas <jelukas89@gmail.com>
2013-03-14 13:38:39 +0100

    Utf8 videos names

--- trainingme/elearning/management/commands/convert.py
+++ trainingme/elearning/management/commands/convert.py
@@ -1,17 +1,20 @@
+# -*- encoding: utf-8 -*-
+from django.utils.encoding import smart_unicode
 from django.core.management.base import BaseCommand, CommandError
 from elearning.models import Video
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
+
+
 class Command(BaseCommand):
     args = '<old_video_path orignal_video_path converted_video_path_mp4  converted_video_path_webm>'
     help = 'Actualizamos el Video'
 
     def handle(self, *args, **options):
-        self.stdout.write(args[0])
-        video = Video.objects.get(original_video_file=args[1])
-        video.original_video_file = args[0]
-        video.converted_video_file_mp4 = args[2]
-        video.converted_video_file_webm = args[3]
+        video = Video.objects.get(original_video_file=smart_unicode(args[1]))
+        video.original_video_file = smart_unicode(args[0])
+        video.converted_video_file_mp4 = smart_unicode(args[2])
+        video.converted_video_file_webm = smart_unicode(args[3])
         video.save()
         user = User.objects.get(pk=video.get_owner_id())
         user.email_user(_('One of Your video is ready'), _('Your video is Ready'), from_email='trainingme@trainingme.net')
