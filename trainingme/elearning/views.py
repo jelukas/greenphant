1ea19b0fa0dba9fc4efdf48d3dc3b26ae3ffa378
Jesus Lucas <jelukas89@gmail.com>
2013-03-25 13:51:49 +0100

    Changed courses at home

--- trainingme/elearning/views.py
+++ trainingme/elearning/views.py
@@ -561,13 +561,13 @@ HOME PAGE
 """
 def home(request):
     cursos_de_prueba = [51,48,47,20,22,29,10,54,14]
-    cursos_mas_visitados = [29,10,54,14]
+    cursos_mas_visitados = [29,10,54,14,23]
     cursos_destacados = [54,13,29,23]
     from django.contrib.auth.forms import AuthenticationForm
     form = AuthenticationForm(request)
     users_count = User.objects.count()
     if request.POST:
-        courses_published = Course.objects.filter(Q(short_description__icontains=request.POST['query']) | Q(title__icontains=request.POST['query']),Q(status__name="evaluation period") | Q(status__name="published"))
+        courses_published = Course.objects.filter(Q(short_description__icontains=request.POST['query']) | Q(title__icontains=request.POST['query']),Q(status__name="evaluation period"))
         featured = Course.objects.filter(id__in=cursos_destacados)
         courses_building = Course.objects.filter(Q(short_description__icontains=request.POST['query']) | Q(title__icontains=request.POST['query']), Q(status__name="building")).exclude(id__in = cursos_de_prueba)
     else:
