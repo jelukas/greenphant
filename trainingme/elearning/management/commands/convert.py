dbfd39a4830535b37f71f54b939949f74163a757
Jesus Lucas <jelukas89@gmail.com>
2013-03-25 12:59:55 +0100

    Update video duration on convert.py

--- trainingme/elearning/management/commands/convert.py
+++ trainingme/elearning/management/commands/convert.py
@@ -4,6 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 from elearning.models import Video
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
+from ...utils.videoutils import get_video_length
 
 
 class Command(BaseCommand):
@@ -15,6 +16,9 @@ class Command(BaseCommand):
         video.original_video_file = smart_unicode(args[0])
         video.converted_video_file_mp4 = smart_unicode(args[2])
         video.converted_video_file_webm = smart_unicode(args[3])
+        path = video.converted_video_file_mp4.path
+        seconds = get_video_length(path)
+        video.duration = seconds
         video.save()
         user = User.objects.get(pk=video.get_owner_id())
         user.email_user(_('One of Your video is ready'), _('Your video is Ready'), from_email='trainingme@trainingme.net')
